7e2e3bd0e7e7dd37cb491135f0656b8130be39a0
samira-sadat48 <samira.sadat48@gmail.com>
2022-04-27 14:02:16 -0600

    Added event listener to add book button - shows form

--- books.js
+++ books.js
@@ -1,3 +1,10 @@
+//DOM Elements
+const openFormTrigger = document.querySelector(".trigger");
+//const closeFormTrigger = document.querySelector(".close");
+const form = document.querySelector(".form-popup");
+
+
+//Library Book Objects
 let myLibrary = [];
 
 function Book(title, author, numOfPages, haveRead) {
@@ -22,7 +29,19 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+//test code
 const book1 = new Book('Hope Never Dies','Andrew Shaffer',301,false);
 console.log(book1.info());
 addBookToLibrary(book1);
 console.log(myLibrary);
+
+//Form pop-up trigger
+openFormTrigger.addEventListener("click", () => {form.classList.add("open")});
+
+//Submit button pressed
+form.addEventListener("submit", (e) => {
+    //create and save new book object using form fields
+    //push to library list
+    //close the form
+    form.classList.remove("open");
+})
