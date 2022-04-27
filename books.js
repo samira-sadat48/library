1b33657d362814b95aaf107e99c605bbdd5a2a18
samira-sadat48 <samira.sadat48@gmail.com>
2022-04-27 14:21:34 -0600

    Created a close button to hide form pop up

--- books.js
+++ books.js
@@ -1,11 +1,13 @@
 //DOM Elements
 const openFormTrigger = document.querySelector(".trigger");
-//const closeFormTrigger = document.querySelector(".close");
-const form = document.querySelector(".form-popup");
+const closeFormTrigger = document.querySelector(".close");
+const formPopUp = document.querySelector(".form-popup");
+const form = document.getElementById("book-form");
 
 
 //Library Book Objects
 let myLibrary = [];
+let newBook;
 
 function Book(title, author, numOfPages, haveRead) {
     this.title = title;
@@ -15,7 +17,7 @@ function Book(title, author, numOfPages, haveRead) {
     this.info = function() {
         let haveReadString;
         if(haveRead){
-            haveIReadString = "already read";
+            haveReadString = "already read";
         }
         else
         {
@@ -36,12 +38,30 @@ addBookToLibrary(book1);
 console.log(myLibrary);
 
 //Form pop-up trigger
-openFormTrigger.addEventListener("click", () => {form.classList.add("open")});
+openFormTrigger.addEventListener("click", () => {formPopUp.classList.add("open")});
+closeFormTrigger.addEventListener("click", () => formPopUp.classList.remove("open"));
+window.addEventListener("click", (e) => {
+    if(e.target === formPopUp) {
+        formPopUp.classList.remove("open")
+    }
+})
 
 //Submit button pressed
 form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let formValues = e.target.elements;
     //create and save new book object using form fields
+    newBook = new Book(
+        formValues.title.value,
+        formValues.author.value,
+        formValues.numOfPages.value,
+        formValues.haveRead.checked
+    )
     //push to library list
+    myLibrary.push(newBook);
+    //reset form
+    form.reset();
     //close the form
-    form.classList.remove("open");
+    formPopUp.classList.remove("open");
+    return false;
 })
