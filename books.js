ccc82c9895484eff9c2a6364ce3e0084f894a142
samira-sadat48 <samira.sadat48@gmail.com>
2022-04-28 12:09:04 -0600

    Fixed the form submit event listener

--- books.js
+++ books.js
@@ -4,9 +4,6 @@ const openFormTrigger = document.querySelector(".trigger");
 const closeFormTrigger = document.querySelector(".close");
 const formPopUp = document.querySelector(".form-popup");
 
-const submitFormTrigger = document.querySelector(".submit-btn");
-
-
 //Library Book Objects
 let myLibrary = [];
 let newBook;
@@ -21,7 +18,6 @@ function Book(title, author, numOfPages, haveRead) {
 //Form pop-up triggers
 openFormTrigger.addEventListener("click", () => {formPopUp.classList.add("open")});
 closeFormTrigger.addEventListener("click", () => formPopUp.classList.remove("open"));
-//submitFormTrigger.addEventListener("click", () => formPopUp.classList.remove("open"));//try to do this in the submit event
 
 window.addEventListener("click", (e) => {
     if(e.target === formPopUp) {
@@ -30,22 +26,24 @@ window.addEventListener("click", (e) => {
 })
 
 //Submit button pressed
-form.addEventListener("submit", (e) => {
-    console.log("Submit event!");
-    e.preventDefault();
-    let formValues = e.target.elements;
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let formValues = event.target.elements;
+
     //create and save new book object using form fields
     newBook = new Book(
         formValues.title.value,
         formValues.author.value,
-        formValues.numOfPages.value,
-        formValues.haveRead.checked
+        formValues.pages.value,
+        formValues.read.checked
     );
+    
     //push to library list
     myLibrary.push(newBook);
+    console.log(myLibrary);
     //reset form
     form.reset();
     //close the form
     formPopUp.classList.remove("open");
     return false;
-})
+});
